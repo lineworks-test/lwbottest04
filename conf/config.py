78c2f600f133861b5d68825821936408c6e5b22e
LINE WORKS <57744269+lineworks-test@users.noreply.github.com>
2019-11-28 11:04:08 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -14,14 +14,14 @@ API_ID = "kr1hLBbxHJiDs"
 SERVER_CONSUMER_KEY = "cqRT31EkE5TFcSZCkIzt"
 
 SERVER_ID = "c2d6d1eb148b4144a9f75abe49faece0"
-SECRET_KEY_NAME = "private_20191127124751.key"
+SECRET_KEY_NAME = "private_20191128110223.key"
 
 # LOCAL ADDRESS
-LOCAL_ADDRESS = "https://pm191127samplebot.herokuapp.com/"
+LOCAL_ADDRESS = "https://lwbottest04.herokuapp.com/"
 
 # DB config
-DB_HOST = "ec2-54-235-248-185.compute-1.amazonaws.com"
-DB_NAME = "d948ducpbs8to4"
-DB_USER = "irxbslbpfagcyw"
+DB_HOST = "ec2-23-21-248-1.compute-1.amazonaws.com"
+DB_NAME = "d6kt29aa13qkhe"
+DB_USER = "dfefvktohojmdd"
 DB_PORT = "5432"
-DB_PASSWORD = "4484cc1abc190bd1bc9c46da75ff2f67df260963ee3e87f0eb7fd2eecd3942d3"
+DB_PASSWORD = "51b78840758caa556171b6a37453d217835af9a2177f22d9ea0b4d8bf416fea3"
