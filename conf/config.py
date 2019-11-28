3e633447889bc94000b53fed4f48508f905e306f
LINE WORKS <57744269+lineworks-test@users.noreply.github.com>
2019-11-28 10:58:48 +0900

    upload 01
    upload 01

---
+++ conf/config.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+# TimeZone
+TZone = "Asia/Tokyo"
+
+# account
+ADMIN_ACCOUNT = "admin@lwbottest04"
+DOMAIN_ID = 153499
+
+# api
+API_ID = "kr1hLBbxHJiDs"
+SERVER_CONSUMER_KEY = "cqRT31EkE5TFcSZCkIzt"
+
+SERVER_ID = "c2d6d1eb148b4144a9f75abe49faece0"
+SECRET_KEY_NAME = "private_20191127124751.key"
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https://pm191127samplebot.herokuapp.com/"
+
+# DB config
+DB_HOST = "ec2-54-235-248-185.compute-1.amazonaws.com"
+DB_NAME = "d948ducpbs8to4"
+DB_USER = "irxbslbpfagcyw"
+DB_PORT = "5432"
+DB_PASSWORD = "4484cc1abc190bd1bc9c46da75ff2f67df260963ee3e87f0eb7fd2eecd3942d3"
